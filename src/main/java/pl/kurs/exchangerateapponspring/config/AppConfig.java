79be6cce1095541c86923bb12f021de877fb30d3
igiurban <109105195+igiurban@users.noreply.github.com>
2023-03-07 17:20:51 +0100

    Added AppConfig and BeanConfig

---
+++ src/main/java/pl/kurs/exchangerateapponspring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package pl.kurs.exchangerateapponspring.config;
+
+public interface AppConfig {
+
+    String API_PAGE = "https://api.fastforex.io";
+    String ENDPOINT = "/fetch-all?from=";
+    String API_PRIVATE_KEY = "&api_key=dbb84340be-a11dee2acb-rqy55m";
+
+
+}
---
+++ src/main/java/pl/kurs/exchangerateapponspring/config/BeanConfig.java
@@ -0,0 +1,23 @@
+package pl.kurs.exchangerateapponspring.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    private static ObjectMapper create() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        return mapper;
+    }
+}
